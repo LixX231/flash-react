6149c97afb338893f9e2bda95a5478a4ed362d11
LixX231 <maksgallymov@gmail.com>
2025-11-19 11:17:23 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Header from './components/Header'
-import Set from './components/Set'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PageSelectSet } from './components/PageSelectSet'
+import { PageViewSet } from './components/PageViewSet'
+import { PageCreateSet } from './components/PageCreateSet'
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Set />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageSelectSet/>} />
+        <Route path="/set" element={<PageViewSet/>} />
+        <Route path="/admin" element={<PageCreateSet/>} />
+        <Route path="*" element={<PageSelectSet/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/PageSelectSet.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./Header";
+
+export function PageSelectSet(){
+    return <div>
+        <Header/>
+        <h2>Выбор сетов</h2>
+    </div>
+}
